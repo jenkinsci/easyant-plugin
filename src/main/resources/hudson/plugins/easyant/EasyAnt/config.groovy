6867e9ea5c58749caa65557a1b0a77219da34686
Jean-Louis Boudart <jeanlouis.boudart@gmail.com>
2013-07-11 09:14:23 +0200

    Starts refactoring UI, switch to groovy for project config (again inspired by ant plugin)

---
+++ src/main/resources/hudson/plugins/easyant/EasyAnt/config.groovy
@@ -0,0 +1,29 @@
+package hudson.plugins.easyant.EasyAnt;
+f=namespace(lib.FormTagLib)
+
+if (descriptor.installations.length != 0) {
+    f.entry(title:_("Ant Version")) {
+        select(class:"setting-input",name:"easyAnt.easyAntName") {
+            option(value:"(Default)", _("Default"))
+            descriptor.installations.each {
+                f.option(selected:it.name==instance?.easyAnt?.name, value:it.name, it.name)
+            }
+        }
+    }
+}
+
+f.entry(title:_("Targets"),field:"targets") {
+    f.expandableTextbox()
+}
+
+f.advanced {
+    f.entry(title:_("Build File"),field:"buildFile") {
+        f.expandableTextbox()
+    }
+    f.entry(title:_("Properties"),field:"properties") {
+        f.expandableTextbox()
+    }
+    f.entry(title:_("Java Options"),field:"easyAntOpts") {
+        f.expandableTextbox()
+    }
+}
